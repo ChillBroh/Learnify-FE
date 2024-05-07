d1109e995b086d57d0ab72c98a441810e21702cd
ChillBroh <isharamadusanka410@gmail.com>
2024-05-08 00:49:56 +0530

    footer responsive error fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import {
-  FaWhatsappSquare,
-  FaFacebookSquare,
-  FaInstagramSquare,
-  FaTwitterSquare,
-} from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0  bg-black p-4 px-10 flex flex-rows justify-between gap-8 text-white">
-      <div className="">
-        <h3 className="text-2xl font-bold text-red-900">
-          Copyright @2023 <span className="text-white">NASA</span>
-        </h3>
-      </div>
-      <div>
-        <div className="flex text-red-800 gap-10 md:w-28">
-          <FaWhatsappSquare size={30} />
-          <FaFacebookSquare size={30} />
-          <FaInstagramSquare size={30} />
-          <FaTwitterSquare size={30} />
+    <div className="lg:fixed bottom-0 left-0 right-0 mt-5  bg-white lg:bg-opacity-20  p-4 px-10 flex flex-rows justify-between gap-8 text-white">
+      <div className="flex flex-row">
+        <div className="text-black"> Copyright @2023 </div>
+        <div>
+          <img src={logo} alt="" className="h-6 w-auto" />
         </div>
       </div>
+      <div></div>
     </div>
   );
 };
