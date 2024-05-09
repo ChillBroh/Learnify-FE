daefd0a3ca06602e3458da28eb4d120282d04685
ChillBroh <isharamadusanka410@gmail.com>
2024-05-09 16:35:50 +0530

    errors fixed

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MenuNav from "../components/MenuNavBar";
+import axios from "../util/AxiosInstance";
+import Loader from "../components/Loader";
 
 const Courses = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getAllCourses = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(
+          "http://localhost:4000/api/guest/course/guest-routes"
+        );
+        setData(res.data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+      getAllCourses();
+    };
+  }, []);
   return (
     <div>
       <MenuNav />
-      Courses
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="lg:px-48 px-12">
+          <div className="text-center text-3xl font-bold text-[#411EE2] pt-10">
+            All Courses In Our WebSite
+          </div>
+          <div className="grid pt-10 lg:grid-cols-4 md:grid-cols-3 gap-3 grid-cols-2">
+            <div className="w-[100%] h-96 bg-red-800">1</div>
+            <div className="w-[100%] h-96 bg-blue-500">2</div>
+            <div className="w-[100%] h-96 bg-red-800">3</div>
+            <div className="w-[100%] h-96 bg-red-800">4</div>
+            <div className="w-[100%] h-96 bg-red-800">5</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
