d5c3722d8645c5a28bc4c3ce4164faf9ba0132d7
X-pose <falconbusiness123@gmail.com>
2024-05-12 18:05:37 +0530

    Admin page is done

---
+++ src/components/admin/ViewUserListModel.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../../util/AxiosInstance'
+
+export default function ViewUserListModel({ open, onClose, userType }) {
+
+    const [userList, setUserList] = useState([])
+  
+
+    useEffect(() => {
+        console.log('User type is : ' + userType)
+        getUserList()
+    }, [userType])
+
+    const getUserList = async () => {
+
+        if (userType === 'instructor') {
+            const res = await axios.get(`user/common/instructor`)
+
+            if (res.status === 200) {
+                setUserList(res.data)
+                console.log(res.data)
+
+            }
+
+        }
+
+        if (userType === 'admin') {
+            const res = await axios.get(`user/common/admin`)
+
+            if (res.status === 200) {
+                setUserList(res.data)
+                console.log(res.data)
+            }
+
+        }
+
+    }
+
+    return (
+        <div onClick={onClose} className={` absolute z-[999] inset-0 flex justify-center items-center ${open ? 'visible bg-black/50 ' : 'invisible'}`}>
+            {/**Actual modal */}
+
+            <div onClick={(e) => e.stopPropagation()} className={`bg-white p-2 items-start flex flex-row w-fit h-fit rounded-md shadow transition-all ${open ? 'opacity-100' : 'opacity-0'}`}>
+
+                <div className=' flex flex-col w-[50vw] h-[50vh] text-[#575757] items-center'>
+                    <div className=' flex py-2 w-full justify-center mb-3'>
+                        <span className={`${userType === 'instructor' ? 'block' : 'hidden'} text-2xl text-[#575757] font-semibold`}>Current Instructors List</span>
+                        <span className={`${userType === 'admin' ? 'block' : 'hidden'} text-2xl text-[#575757] font-semibold`}>Current Administrators List</span>
+                    </div>
+                    {userList.map((item) => (
+                        <div className='w-[90%] flex my-2 justify-between py-2 px-4 border-2 text-[#575757] border-[#575757] font-FuturaMdBt rounded-xl' key={item.id}>
+                            <span>{item.userName}</span>
+                            <span> Mobile no : {item.mobileNo ? item.mobileNo:'No mobile Number yet'}</span>
+                        </div>
+                    ))}
+
+                </div>
+                <div className=' text-4xl  cursor-pointer justify-center items-center flex text-[#575757]'>
+                    <span onClick={onClose}>&times;</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PendingCoursesPanel from "../../components/admin/PendingCourses";
 import UserDetailsPanel from "../../components/admin/UserDetailsPanel";
+import AddUserPanel from "../../components/admin/AddUserPanel";
+import ViewUserListModel from "../../components/admin/ViewUserListModel";
 
 const Home = () => {
 
+  const[openUserList, setUserListOpen] = useState(false)
+  const[userRole, setUserType] = useState('')
 
   return (
 
@@ -11,13 +15,21 @@ const Home = () => {
       <div className=" w-1/4 h-full bg-zinc-100">
         <UserDetailsPanel/>
       </div>
-      <div className=" w-1/3 h-full bg-slate-600 px-10 py-5 flex flex-col justify-evenly">
-        <div className=" w-full h-[45%] bg-green-500 "></div>
-        <div className=" w-full h-[45%] bg-green-200 ">
-
+      <div className=" w-[36vw] h-full   py-5 flex flex-col justify-evenly">
+        <div className=" w-full h-[60%]  ">
+          <AddUserPanel/>
+        </div>
+        <div className=" w-full h-[40%] flex flex-col items-center justify-evenly font-FuturaMdBt font-semibold text-xl text-[#575757]">
+          <div onClick={() =>{ setUserType('instructor'); setUserListOpen(true);}} className=" flex justify-center bg-white py-5 rounded-xl w-[80%] my-5 border-2 border-[#575757] hover:border-green-500 hover:bg-gray-200 ">
+            <span >View Instructors</span>
+          </div>
+          <div onClick={() => { setUserType('admin'); setUserListOpen(true); }} className=" flex justify-center bg-white py-5 rounded-xl w-[80%] my-5 border-2 border-[#575757] hover:border-green-500 hover:bg-gray-200 ">
+            <span >View Admins</span>
+          </div>
         </div>
       </div>
-      <div className=""><PendingCoursesPanel/></div>
+      <div className=" flex justify-end"><PendingCoursesPanel/></div>
+      <ViewUserListModel open={openUserList} onClose={() => setUserListOpen(false)} userType={userRole} />
     </div>
   )
 
