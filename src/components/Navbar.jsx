48abbba5a4e88b2cd93f9fc19980961887031040
Tharushi Dissanayake <it21319174@my.sliit.lk>
2024-05-15 08:07:19 +0530

    Modified file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,9 +36,10 @@ const Navbar = () => {
         text: "Successfully LoggedOut!!",
       });
       navigate("/login");
+
     } catch (error) {
       console.error("Logout failed:", error);
-      // Show error message
+      // Show error Message
       Swal.fire({
         icon: "error",
         title: "Logout Failed",
